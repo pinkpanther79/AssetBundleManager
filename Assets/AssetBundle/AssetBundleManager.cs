d5e84e0424881504415acc030aea0d2943b026a1
pinkpanther <pinkpanther79@trinitygames.co.kr>
2019-01-11 18:33:36 +0900

    Add Sample Script / Resource Add AssetBundleManager Script Add SampleScene

---
+++ Assets/AssetBundle/AssetBundleManager.cs
@@ -0,0 +1,13 @@
+﻿namespace AssetBundle
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Networking;
+    
+    public static class AssetBundleManager
+    {
+        public static bool UseAssetBundle = false;
+
+    }
+}
---
+++ Assets/Sample/Sample.cs
@@ -0,0 +1,30 @@
+﻿namespace AssetBundle
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage DownloadedImage = null;
+
+        private Texture Texture = null;
+
+        private string ResourcePath = "SampleTexture";
+
+        public void OnClickResoureLoad()
+        {
+            Texture = UnityEngine.Resources.Load(ResourcePath) as Texture;
+
+            if (Texture != null)
+            {
+                DownloadedImage.texture = Texture;
+            }
+        }
+
+        public void OnClickResourceUnload()
+        {
+            UnityEngine.Resources.UnloadAsset(Texture);
+        }
+    }
+}
